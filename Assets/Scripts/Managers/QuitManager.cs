8d5812f9dee5feb8c71176aba79f04b465e6df46
kcoderhtml <92754843+kcoderhtml@users.noreply.github.com>
2022-05-21 14:39:15 -0400

    Made QuitManager a coroutine

--- Assets/Scripts/Managers/QuitManager.cs
+++ Assets/Scripts/Managers/QuitManager.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class QuitManager : MonoBehaviour
 {
+    public LevelLoader levelLoader;
+
     public void QuitGame()
     {
+        StartCoroutine(QuitGameCoroutine());
+    }
+    IEnumerator QuitGameCoroutine()
+    {
+        levelLoader.transition.SetTrigger("Start");
+        yield return new WaitForSeconds(levelLoader.transitionTime);
+
         #if UNITY_EDITOR
 
         UnityEditor.EditorApplication.isPlaying = false;
 
         #endif
-        
+
         Application.Quit();
     }
 
