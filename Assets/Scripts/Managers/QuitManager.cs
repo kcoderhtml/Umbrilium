2778d5b0a3ddb94be171077571aac4d42877f31e
kcoderhtml <92754843+kcoderhtml@users.noreply.github.com>
2022-05-21 14:28:37 -0400

    Added quit manager script

--- Assets/Scripts/Managers/QuitManager.cs
+++ Assets/Scripts/Managers/QuitManager.cs
@@ -6,6 +6,12 @@ public class QuitManager : MonoBehaviour
 {
     public void QuitGame()
     {
+        #if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        #endif
+        
         Application.Quit();
     }
 
