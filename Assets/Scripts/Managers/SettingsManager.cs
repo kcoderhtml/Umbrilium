a369ac10b2b348fee4e991da28f85af43f5428f3
kcoderhtml <92754843+kcoderhtml@users.noreply.github.com>
2022-05-22 17:50:58 -0400

    Finished Resolution Code and Added back button

--- Assets/Scripts/Managers/SettingsManager.cs
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -2,10 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsManager : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "Hz";
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.width &&
+                resolutions[i].height == Screen.height &&
+                resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("volume", volume);
@@ -20,4 +54,5 @@ public class SettingsManager : MonoBehaviour
     {
         Screen.fullScreen = isFullscreen;
     }
+    
 }
